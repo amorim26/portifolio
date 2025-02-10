b537cc1d5d39a3a641faf6569e345ad291e7ef27
Nathaly <nathalywerner262@gmail.com>
2025-02-10 20:26:54 -0300

    AULA 10/02/25

--- index.php
+++ index.php
@@ -5,10 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifólio</title>
 
-
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+            <div class="logo">
+
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+
+            </div>
+            <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Produtos</li>
+                    <li>Portifolio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+             </ul>
+
+             <ol>
+
+            </ol>
+
+        </nav>
+        <div class="title">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluçoes em Ti</h1>
+            <p class="paragrafo">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta placeat reiciendis distinctio sapiente asperiores fuga!</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">FaceBook</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
+        </div>
+
+        
+    </header>
+
+
+</div>
     
 </body>
 </html>
